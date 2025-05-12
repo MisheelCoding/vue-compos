94b38d7e657fca872736fd1db43ff106d3a8c43e
Misheel <misheelcoding@gmail.com>
2025-05-12 17:45:55 +0300

    add login form

---
+++ src/views/github/FormLogin.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <!-- Пример добавления -->
+    <input type="text" placeholder="Username" class="border" />
+    <input type="password" placeholder="Password" class="border" />
+  </div>
+</template>
+
+<script setup></script>
