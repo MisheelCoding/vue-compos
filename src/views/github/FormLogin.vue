61aee076da6e3d1d37ca31e1928f289b87b1ef5c
Misheel <misheelcoding@gmail.com>
2025-05-12 22:10:49 +0300

    i am second devoleper

--- src/views/github/FormLogin.vue
+++ src/views/github/FormLogin.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <!-- Пример добавления -->
+    <p class="text-green-500 text-sm">Я второй разработчик</p>
     <input type="text" placeholder="Username" class="border" />
     <input type="password" placeholder="Password" class="border" />
   </div>
