c88099a58d76ce210af4de9c190edae16fe128a4
Misheel <misheelcoding@gmail.com>
2025-05-29 01:53:37 +0300

    Feat: Add password input

--- src/views/login/LoginForm.vue
+++ src/views/login/LoginForm.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Login Form</h1>
+    <form action="#">
+      <input type="password" name="password" id="password" placeholder="write password..." />
+    </form>
   </div>
 </template>
 
